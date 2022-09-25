09b920bc9ff37672c5ea648e990cd38c606fd4d5
Darlington Arinze <darlingtonarinze1995@gmail.com>
2022-09-25 23:34:43 +0100

    add async-await syntax, initialize newPerson to empty string

--- script/index.js
+++ script/index.js
@@ -1,24 +1,42 @@
-function main() {
-  let img = [
-    "https://loremflickr.com/320/240/paris",
-    "https://loremflickr.com/320/240/dog",
-    "https://dummyimage.com/300",
-    "https://placekitten.com/640/360",
-    "https://loremflickr.com/320/240",
-    "https://loremflickr.com/320/240/brazil,rio",
-    "https://loremflickr.com/320/240/dog",
-    "https://loremflickr.com/320/240/paris,girl/all",
-    "https://placekitten.com/640/360",
-    "https://loremflickr.com/320/240/paris",
-  ];
+async function main() {
+ 
+ const apiUrl = await fetch("https://swapi.dev/api/people");
 
-  let starWarsPerson = document.querySelector("#placeHolder");
+  //fetch error handling
+ if (!apiUrl.ok) {
+  const message = `An error has occured: ${response.status}`;
+  throw new Error(message);
+}
 
-  let newPerson;
+/* 
+modified the script to include async/await fuctionality
+*/
 
-  fetch("https://swapi.dev/api/people")
-    .then((response) => response.json())
-    .then((data) => {
+    const response = await apiUrl.json();
+    // console.log(response);
+
+    return response;
+  };
+
+    main().then((data) => {
+
+      let img = [
+        "https://loremflickr.com/320/240/dog",
+        "https://loremflickr.com/320/240",
+        "https://dummyimage.com/300",
+        "https://placekitten.com/640/360",
+        "https://loremflickr.com/320/240/paris",
+        "https://loremflickr.com/320/240/brazil,rio",
+        "https://loremflickr.com/320/240/paris,girl/all",
+        "https://placekitten.com/640/360",
+        "https://loremflickr.com/320/240/paris",
+        "https://loremflickr.com/320/240/dog",
+      ];
+    
+      let starWarsPerson = document.querySelector("#placeHolder");
+    
+      let newPerson = "";
+
       //gets the results array from the json object
       let arr = data.results;
 
@@ -36,29 +54,34 @@ function main() {
 
       //this code block generates the list of names and their properties and attaches it to the html file
       for (let i = 0; i < newArr.length; i++) {
+
+        // document.querySelector('#text').parentElement.removeChild('#text');
+
         newPerson += `
                   <div class="card">
-                    <img src="${newArr[i].image}" alt="random image">
+
+                    <img src=${newArr[i].image} alt="random image">
+
                     <div class="personInfo">
                       <h3 class="name">${newArr[i].name}</h3>
                     
-                      <p class="birth_year showMe">${newArr[i].height}</p>
-                      <p class="gender showMe">${newArr[i].gender}</p>
-                      
+                      <p class="birth_year showMe"><strong>Height:</strong>${newArr[i].height}</p>
+                      <p class="gender showMe"><strong>Gender:</strong>${newArr[i].gender}</p>
                     </div>
+
                   </div>
                     `;
       }
 
-      starWarsPerson.innerHTML = newPerson;
+      return starWarsPerson.innerHTML = newPerson;
 
-      return newPerson;
     })
-    .catch((err) => {
-      console.error(err);
-    });
-}
+    .catch((err, message) => {
+      console.error(err, message);
+    }); 
 
 main();
 
 // module.exports = { main };
+
+
