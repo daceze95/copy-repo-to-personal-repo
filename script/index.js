3ccc267e0098d7a54366d5dfd2917bfdbfd4a15c
Darlington Arinze <darlingtonarinze1995@gmail.com>
2022-09-25 15:16:04 +0100

    first draft of project

--- script/index.js
+++ script/index.js
@@ -1,3 +1,58 @@
-function main() {}
+function main() {
 
-module.exports = { main }
+ let img = ['https://loremflickr.com/320/240/paris',
+ 'http://dummy-images.com/abstract/dummy-454x280-RedDots.jpg', 'https://dummyimage.com/300',
+ 'https://placekitten.com/640/360', 'https://loremflickr.com/320/240', 'https://loremflickr.com/320/240/brazil,rio', 'http://dummy-images.com/sport/dummy-454x280-GolfBalls.jpg', 'http://dummy-images.com/abstract/dummy-454x280-Goemetry.jpg', 'http://dummy-images.com/abstract/dummy-454x280-RedDots.jpg', 'http://dummy-images.com/sport/dummy-454x280-GolfBalls.jpg'];
+
+  let starWarsPerson = document.querySelector('#placeHolder');
+
+  let newPerson;
+
+     fetch("https://swapi.dev/api/people")
+      .then((response) => response.json())
+          .then((data) => {
+
+            //gets the results array from the json object
+              let arr = data.results; 
+
+              //add image into array object 
+            for(let x in arr){
+              if(arr){
+                arr[x].image = img[x];
+              };
+            }
+
+            //use destructuring and array method to filter the elem in the array.
+           let newArr = arr.map(({name, height, gender,image}) => {
+              return {name, height, gender, image};
+             });
+
+             //this code block generates the list of names and their properties and attaches it to the html file
+              for(let i = 0; i < newArr.length; i++){
+                  newPerson += `
+                  <div class="card">
+                    <img src="${newArr[i].image}" alt="random image">
+                    <div class="personInfo">
+                      <h3 class="name">${newArr[i].name}</h3>
+                    
+                      <p class="birth_year showMe">${newArr[i].height}</p>
+                      <p class="gender showMe">${newArr[i].gender}</p>
+                      
+                    </div>
+                  </div>
+                    `;
+                }
+
+                starWarsPerson.innerHTML = newPerson;
+
+                return newPerson;
+
+            }).catch((err) => {
+              console.error(err);
+            });
+          };
+
+          main();
+
+// module.exports = { main };
+              
