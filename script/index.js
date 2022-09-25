abe6c5162a4770baeeaa8ced65ccca0a0febcde0
Darlington Arinze <darlingtonarinze1995@gmail.com>
2022-09-25 16:38:55 +0100

    update some image url bcos of failed rendering during deploy

--- script/index.js
+++ script/index.js
@@ -1,35 +1,42 @@
 function main() {
+  let img = [
+    "https://loremflickr.com/320/240/paris",
+    "https://loremflickr.com/320/240/dog",
+    "https://dummyimage.com/300",
+    "https://placekitten.com/640/360",
+    "https://loremflickr.com/320/240",
+    "https://loremflickr.com/320/240/brazil,rio",
+    "https://loremflickr.com/320/240/dog",
+    "https://loremflickr.com/320/240/paris,girl/all",
+    "https://placekitten.com/640/360",
+    "https://loremflickr.com/320/240/paris",
+  ];
 
- let img = ['https://loremflickr.com/320/240/paris',
- 'http://dummy-images.com/abstract/dummy-454x280-RedDots.jpg', 'https://dummyimage.com/300',
- 'https://placekitten.com/640/360', 'https://loremflickr.com/320/240', 'https://loremflickr.com/320/240/brazil,rio', 'http://dummy-images.com/sport/dummy-454x280-GolfBalls.jpg', 'http://dummy-images.com/abstract/dummy-454x280-Goemetry.jpg', 'http://dummy-images.com/abstract/dummy-454x280-RedDots.jpg', 'http://dummy-images.com/sport/dummy-454x280-GolfBalls.jpg'];
-
-  let starWarsPerson = document.querySelector('#placeHolder');
+  let starWarsPerson = document.querySelector("#placeHolder");
 
   let newPerson;
 
-     fetch("https://swapi.dev/api/people")
-      .then((response) => response.json())
-          .then((data) => {
+  fetch("https://swapi.dev/api/people")
+    .then((response) => response.json())
+    .then((data) => {
+      //gets the results array from the json object
+      let arr = data.results;
 
-            //gets the results array from the json object
-              let arr = data.results; 
+      //add image into array object
+      for (let x in arr) {
+        if (arr) {
+          arr[x].image = img[x];
+        }
+      }
 
-              //add image into array object 
-            for(let x in arr){
-              if(arr){
-                arr[x].image = img[x];
-              };
-            }
+      //use destructuring and array method to filter the elem in the array.
+      let newArr = arr.map(({ name, height, gender, image }) => {
+        return { name, height, gender, image };
+      });
 
-            //use destructuring and array method to filter the elem in the array.
-           let newArr = arr.map(({name, height, gender,image}) => {
-              return {name, height, gender, image};
-             });
-
-             //this code block generates the list of names and their properties and attaches it to the html file
-              for(let i = 0; i < newArr.length; i++){
-                  newPerson += `
+      //this code block generates the list of names and their properties and attaches it to the html file
+      for (let i = 0; i < newArr.length; i++) {
+        newPerson += `
                   <div class="card">
                     <img src="${newArr[i].image}" alt="random image">
                     <div class="personInfo">
@@ -41,18 +48,17 @@ function main() {
                     </div>
                   </div>
                     `;
-                }
+      }
 
-                starWarsPerson.innerHTML = newPerson;
+      starWarsPerson.innerHTML = newPerson;
 
-                return newPerson;
+      return newPerson;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
 
-            }).catch((err) => {
-              console.error(err);
-            });
-          };
-
-          main();
+main();
 
 // module.exports = { main };
-              
